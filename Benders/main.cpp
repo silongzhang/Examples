aa43f3dc9ab0bd8d5691d46392c3290c951141a0
silongzhang <silongzhang1314@163.com>
2022-09-03 21:38:56 +0800

    save

--- Benders/main.cpp
+++ Benders/main.cpp
@@ -4,8 +4,25 @@
 
 int main(int argc, char** argv) {
 	try {
-		if (argc < 3) throw exception();
+		ParameterTest parameter;
+		parameter.seed = 11;
+		parameter.NInt = 1e3;
+		parameter.NCont = 1e4;
+		parameter.MaxUBInt = 10;
+		parameter.MaxUBCont = 100;
+		parameter.ratioInfUBInt = 0.1;
+		parameter.ratioInfUBCont = 0.1;
+		parameter.minimization = true;
+		parameter.rangeObjCoefInt = parameter.rangeObjCoefCont = parameter.rangeConsCoefInt = parameter.rangeConsCoefCont = { 1,100 };
+		parameter.sparsity = 0.2;
+		parameter.redundancy = 0.05;
+		parameter.ratioEq = 0.05;
+		parameter.ratioLe = 0.25;
+		parameter.ratioGe = 0.7;
+		parameter.NCpCons = 1e3;
+		parameter.NItCons = 1e2;
 
+		Instance instance = parameter.generateInstance();
 	}
 	catch (const exception& exc) {
 		printErrorAndExit("main", exc);
